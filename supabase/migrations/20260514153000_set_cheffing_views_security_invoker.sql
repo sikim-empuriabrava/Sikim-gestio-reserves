e1010ceb1a47411cb28391e85d2b97889f922fbe
urivinyoles <urivinyoles@gmail.com>
2026-05-14 15:38:22 +0200

    Set Cheffing views to security invoker

---
+++ supabase/migrations/20260514153000_set_cheffing_views_security_invoker.sql
@@ -0,0 +1,3 @@
+ALTER VIEW public.v_cheffing_subrecipe_cost SET (security_invoker = true);
+ALTER VIEW public.v_cheffing_dish_items_cost SET (security_invoker = true);
+ALTER VIEW public.v_cheffing_menu_engineering_dish_cost SET (security_invoker = true);
