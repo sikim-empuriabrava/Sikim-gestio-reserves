b298a678eace0c539517ea8853037f15c0bcea99
urivinyoles <urivinyoles@gmail.com>
2026-05-14 18:52:32 +0200

    Set operational views to security invoker

---
+++ supabase/migrations/20260514165000_set_operational_views_security_invoker.sql
@@ -0,0 +1,6 @@
+ALTER VIEW public.v_day_status SET (security_invoker = true);
+ALTER VIEW public.v_maintenance_daily_detail SET (security_invoker = true);
+ALTER VIEW public.v_daily_room_occupancy SET (security_invoker = true);
+ALTER VIEW public.v_daily_staffing_summary SET (security_invoker = true);
+ALTER VIEW public.v_group_events_daily_detail SET (security_invoker = true);
+ALTER VIEW public.v_group_events_calendar_sync SET (security_invoker = true);
